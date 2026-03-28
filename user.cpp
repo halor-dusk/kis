0351a4fd551ab86c80066c434861f09244baa079
halor-dusk <sidneyenzorodriguesdasilva@gmail.com>
2026-03-28 19:41:21 +0000

    remove old files

--- user.cpp
+++
@@ -1,8 +0,0 @@
-#include "user.h"
-
-User::User(const QString &cardId, const QString &firstName, const QString &lastName, int balance)
-    : cardId(cardId)
-    , firstName(firstName)
-    , lastName(lastName)
-    , balance(balance)
-{}
